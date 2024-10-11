f3a3c7aa282d74a95a7d5cd56ffc9c3dcf6d3a5f
jorge_gabriel <thebatman.jorge@gmail.com>
2024-10-10 22:06:58 -0300

    iniciando projeto da api de mercado

---
+++ api_mercado/database.sql
@@ -0,0 +1,25 @@
+-- comandos dll estrutura do banco de dados mercado
+CREATE TABLE produto(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(9,2),
+    status BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE cliente(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    telefone VARCHAR (25),
+    status BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE pedido{
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    PRIMARY KEY(id_cliente),
+    FOREIGN KEY(id_cliente) REFERENCES cliente(id),
+
+    id_produto INT NOT NULL,
+    PRIMARY KEY(id_produto),
+    FOREIGN KEY(id_produto) REFERENCES produto(id),
+};
