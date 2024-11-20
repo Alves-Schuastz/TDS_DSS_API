c4bfd97f1609dec5f992d2efe1b918940f759c43
jorge_gabriel <thebatman.jorge@gmail.com>
2024-11-19 21:57:49 -0300

    componentes e rotas

--- origins/src/App.jsx
+++ origins/src/App.jsx
@@ -1,13 +1,23 @@
-import Teste from "./assets/teste"
+import axios from "axios";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Alunos from "./pages/Alunos";
+import Professores from "./pages/Professor";
+import Salas from "./pages/Salas";
+
+
 
 function App() {
-
-  return (
-    <>
-      <h1>hello world!</h1>
-      <Teste />!""
-    </>
-  )
+    return(
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/Alunos" element={<Alunos/>} ></Route>
+                    <Route path="/Professores" element={<Professores/>} ></Route>
+                    <Route path="/Salas" element={<Salas/>} ></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
